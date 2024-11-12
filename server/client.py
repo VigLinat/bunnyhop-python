cf47f16417254140cdc0dc8dc6bf4c2f9aa5aced
VigLinat <prokhorov1099@gmail.com>
2024-11-12 22:29:53 +0300

    Added experimental support of SQLite3 database storage.

--- server/client.py
+++ server/client.py
@@ -2,7 +2,14 @@ import bh_message.bh_message
 from util.helper import log
 
 from websockets.asyncio.client import ClientConnection
+from websockets.exceptions import ConnectionClosed
 from dataclasses import dataclass
+import hashlib
+import time
+
+import sqlite3
+import os.path
+import weakref
 
 class Room:
     def __init__(self, name: str):
@@ -17,9 +24,11 @@ class Room:
             self.clients.remove(client)
 
     async def broadcast(self, sender: Client, text: str):
+        text_message = f'{sender.name}: {text}'
+        db_manager.new_message(sender.name, text, self.name)
         for client in self.clients:
-            if (client != sender):
-                await client.push(text)
+            if (client != sender): 
+                await client.push(text_message)
 
     def get_room(name: str) -> Room:
         return Room._all_rooms[name]
@@ -38,20 +47,17 @@ Room._all_rooms = {'global': Room._global_room}
 class Client:
     def __init__(self, conn: ClientConnection):
         self.conn: ClientConnection = conn
-        self.name: str = "anon" # TODO
+        hashstr = hashlib.sha384(bytes(str(time.time()), 'latin-1'), usedforsecurity=False)
+        self.name: str = f'anon{hashstr.hexdigest()[-5:]}'
         self.room: Room = None
 
     def register(self):
         if self.room != None:
             self.room.register(self)
+
     def unregister(self):
         if self.room != None:
             self.room.unregister(self)
-
-    async def broadcast(self, text: str):
-        if self.room != None:
-            await self.room.broadcast(self, text)
-        pass
 
     def switch_room(self, new_room: Room):
         self.unregister()
@@ -59,21 +65,46 @@ class Client:
         self.register()
 
     async def push(self, text: str):
-        await self.conn.send(text)
+        try:
+            await self.conn.send(text)
+        except ConnectionClosed:
+            self.unregister()
+
+
+    def handle_join_room(self, room_name: str):
+        try:
+            room = Room.get_room(room_name)
+        except KeyError:
+            return
+        self.switch_room(room)
+
+    def handle_create_room(self, room_name: str):
+        room = Room.add_room(room_name)
+        self.switch_room(room)
+
+    async def handle_send_message(self, message: str):
+        if self.room != None: await self.room.broadcast(self, message)
 
     async def handle_message(self, input: str):
         message = bh_message.bh_message.from_json(input)
         if message is None: return
 
         match message.type:
-            case 'text': await self.broadcast(message.body)
-            case 'join':
-                room_name = message.body
-                try:
-                    room = Room.get_room(room_name)
-                except KeyError:
-                    return
-                self.switch_room(room)
-            case 'create':
-                room = Room.add_room(message.body)
-                self.switch_room(room)
+            case 'text': await self.handle_send_message(message.body)
+            case 'join': self.handle_join_room(message.body)
+            case 'create': self.handle_create_room(message.body)
+
+class DatabaseManager:
+    def __init__(self, db_string: str, userexpand = True):
+        self.db_con = sqlite3.connect(os.path.expanduser(db_string) if userexpand else db_string)
+
+    def new_message(self, sender_name: str, message: str, room_name: str):
+        cur = self.db_con.cursor()
+        cur.execute("INSERT INTO message VALUES(?, ?, ?, datetime('now'))", (sender_name, message, room_name))
+        self.db_con.commit()
+
+    def free(self):
+        self.db_con.close()
+
+db_manager = DatabaseManager("~/.bunnyhop/storage")
+weakref.finalize(db_manager, DatabaseManager.free, db_manager)
--- server/server.py
+++ server/server.py
@@ -19,6 +19,35 @@ async def handle_new_client(websocket: ClientConnection):
         helper.log(f'[{remote}]: {message}')
         await new_client.handle_message(message)
 
+def create_database():
+    """
+    1. connect to the database
+    2. Check if table 'message' exist
+    3. If it does, exit
+    4. Otherwise, create it
+    """
+    import sqlite3
+    import os.path
+    from pathlib import Path
+
+    app_dir = os.path.expanduser("~/.bunnyhop") 
+    path = Path(app_dir)
+    if not path.exists():
+        path.mkdir(parents=True)
+
+    con = sqlite3.connect(f'{app_dir}/storage')
+    cur = con.cursor()
+    res = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='message';")
+    if res.fetchone() is None:
+        cur.execute("""CREATE TABLE message(
+            sender TEXT,
+            message TEXT,
+            room TEXT,
+            time_stamp TEXT
+            )""")
+    con.close()
+
 async def run():
+    create_database()
     async with serve(handler, "", 8080, ping_interval=10, ping_timeout=20):
         await asyncio.get_running_loop().create_future() # run forever
